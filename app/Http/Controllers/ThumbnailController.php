cb18570b7d5806e7e9e1f1e1c66dad7e7c797e8a
Norvin Kyle Perez <115744078+Aztromi@users.noreply.github.com>
2026-01-05 12:18:26 +0800

    Fixed Thumbnails

--- app/Http/Controllers/ThumbnailController.php
+++ app/Http/Controllers/ThumbnailController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Carbon;
